88865449a7e5c1d65e7d67eefef760e119d0a791
Siddhanth Kheria <sidkheria@gmail.com>
2025-01-25 20:33:59 +0000

    fixed build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
